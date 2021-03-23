84bb359dc539d70cd979bee342b95c9296167cf9
sarthak143 <sarthak.tiet@gmail.com>
2021-03-23 23:07:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# meanstack1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/meanstack1)
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { HeaderComponent } from "./header/header.component";
+import { PostListComponent } from "./posts/post-list/post-list.component";
+import { AngularMaterialModule } from "./material.module";
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularMaterialModule,
+    BrowserAnimationsModule
+  ],
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
