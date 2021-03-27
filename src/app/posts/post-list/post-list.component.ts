7fb87ee87035c6ac5c7d376fcd699d66349024bb
sarthak143 <sarthak.tiet@gmail.com>
2021-03-27 16:35:46 +0530

    3 files modified

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -29,7 +29,9 @@ export class PostListComponent implements OnInit, OnDestroy {
         this.posts = posts;
       });
   }
-
+  onDelete(postId: string) {
+    this.postsService.deletePost(postId);
+  }
   ngOnDestroy() {
     this.postsSub.unsubscribe();
   }
